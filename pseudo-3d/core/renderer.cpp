2bde79c9b35c76bb143fc2105b7041d6ed97cf98
sudo-m3z1m <nizilplay2004@gmail.com>
2026-01-08 20:41:46 +0300

    Added new renderer abstraction

---
+++ pseudo-3d/core/renderer.cpp
@@ -0,0 +1,8 @@
+//
+//  renderer.cpp
+//  pseudo-3d
+//
+//  Created by MeZiM Tabla on 1/8/26.
+//
+
+#include "renderer.hpp"
---
+++ pseudo-3d/core/renderer.hpp
@@ -0,0 +1,60 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#define INIT_SCREEN_WIDTH 640
+#define INIT_SCREEN_HEIGHT 640 //It needs to be in config
+#define TEXTURE_SIZE 64
+	
+#include <stdio.h>
+#include <SDL3/SDL.h>
+
+#include "color.hpp"
+
+class Renderer
+{
+private:
+//	Camera* current_camera;
+	SDL_Renderer* application_renderer;
+	SDL_Surface* color_buffer;
+	SDL_Surface* textures_buffer;
+	
+	int screen_width, screen_height;
+	
+public:
+	Renderer();
+	~Renderer();
+	
+	void draw_pixel_in_buffer(int x, int y, Color color);
+	void render_buffer();
+};
+
+	//typedef struct Renderer
+	//{
+	//	SDL_Renderer* main_renderer;
+	//	SDL_Surface* textures_buffer[32];
+	//	SDL_Window* main_window;
+	//	int width, height;
+	//
+	//	SDL_Surface* color_buffer;
+	//} Renderer;
+	//
+	//Renderer* initialize_renderer(int width, int height);
+	//
+	//void render_buffer(Renderer renderer);
+	//void draw_pixel_in_buffer(Renderer renderer, int x, int y, Color color);
+	//void draw_3d(Renderer renderer, Player player);
+	//void draw_horizontal_surfaces_3d(Renderer renderer, Player player);
+	//
+	//void draw_texture_column(Renderer renderer,
+	//						 Vector2D draw_position,
+	//						 float wall_height,
+	//						 float texture_delta,
+	//						 int texture_index);
+	//
+	//static void draw_texture_row(Renderer renderer,
+	//							 Player player,
+	//							 SDL_Surface* texture,
+	//							 float distance,
+	//							 int screen_delta);
+	//
+	#endif
