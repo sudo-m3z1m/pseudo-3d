67eb3b20e674c99f5545e38742ba9bcbafc47857
sudo-m3z1m <nizilplay2004@gmail.com>
2026-03-19 19:17:38 +0300

    Added texture buffer h file

---
+++ pseudo-3d/common/render/texture-buffer/texture-buffer.hpp
@@ -0,0 +1,29 @@
+#ifndef TEXTURE_BUFFER_HPP
+#define TEXTURE_BUFFER_HPP
+
+#include <stdio.h>
+#include <SDL3/SDL.h>
+#include <vector>
+#include <string>
+
+#include "vector2D.hpp"
+#include "color.hpp"
+
+class TextureBuffer
+{
+private:
+	std::vector<SDL_Surface*> buffer;
+	int buffer_size;
+	
+public:
+	TextureBuffer();
+	TextureBuffer(int buffer_size);
+	~TextureBuffer();
+	
+	int load_texture(std::string& path);
+	void remove_texture(int tid);
+	SDL_Surface* get_texture_surface(int tid);
+	Color get_texture_pixel(int tid, Vector2D<int>& pixel_pos);
+};
+
+#endif
