ecb1fbc1bd6a53c4a6debb9fe44e4b96d61bf6c6
sudo-m3z1m <nizilplay2004@gmail.com>
2026-04-09 14:13:33 +0300

    Renamed file-server

---
+++ pseudo-3d/common/io-files-server/file-server/file-server.cpp
@@ -0,0 +1,1 @@
+#include "file-server.hpp"
--- pseudo-3d/common/io-files-server/file-server/file-server.hpp
+++ pseudo-3d/common/io-files-server/file-server/file-server.hpp
@@ -1,24 +1,23 @@
-#ifndef IO_FILES_SERVER_HPP
-#define IO_FILES_SERVER_HPP
+#ifndef FILE_SERVER_HPP
+#define FILE_SERVER_HPP
 
 #include <stdio.h>
 #include <string>
 
-class IOFilesServer
+class FileServer
 {
 private:
 	//TODO: Just make a cast of level-server and texture buffer and save it to bin file.
 	//TODO: JSON will added soon after it
 	
 public:
-	IOFilesServer();
-	~IOFilesServer();
-	
+	FileServer();
+	~FileServer();
 //	void write_package_json(std::string name, std::string json);
 //	void write_package_bin(std::string name);
 	
-	void read_packages_json(std::string path);
-	void read_packages_bin(std::string path);
+//	void read_packages_json(std::string path);
+//	void read_packages_bin(std::string path);
 };
 
 #endif
