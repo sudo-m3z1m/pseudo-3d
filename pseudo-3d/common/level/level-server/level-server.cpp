cb619a639d855470dca3396d7a14572b663c34f7
sudo-m3z1m <nizilplay2004@gmail.com>
2026-04-15 14:58:46 +0300

    Updated level server with cameras

--- pseudo-3d/common/level/level-server/level-server.cpp
+++ pseudo-3d/common/level/level-server/level-server.cpp
@@ -4,13 +4,24 @@ LevelServer::LevelServer()
 {
 	level_polygons = std::vector<ShapeComponent>();
 	sectors = std::vector<Sector>();
+	cameras = std::vector<Camera*>();
 }
 
-LevelServer::~LevelServer() { return; }
-
-std::vector<ShapeComponent> LevelServer::get_levels_shapes()
+LevelServer::~LevelServer()
 {
-	return level_polygons;
+	cameras.clear();
+}
+
+void LevelServer::add_camera(Camera* camera)
+{
+	cameras.push_back(camera);
+}
+
+Camera* LevelServer::get_camera(int index)
+{
+	if(index >= cameras.size()) return nullptr;
+	
+	return cameras[index];
 }
 
 void LevelServer::add_new_polygon(ShapeComponent new_polygon)
@@ -18,6 +29,18 @@ void LevelServer::add_new_polygon(ShapeComponent new_polygon)
 	if (new_polygon.type == DOT) return;
 	
 	level_polygons.push_back(new_polygon);
+}
+
+ShapeComponent LevelServer::get_levels_shape(int index)
+{
+	if(index >= level_polygons.size()) return ShapeComponent();
+	
+	return level_polygons[index];
+}
+
+std::vector<ShapeComponent> LevelServer::get_levels_shapes()
+{
+	return level_polygons;
 }
 
 void LevelServer::add_new_sector(Sector new_sector)
--- pseudo-3d/common/level/level-server/level-server.hpp
+++ pseudo-3d/common/level/level-server/level-server.hpp
@@ -6,20 +6,26 @@
 
 #include "shape-component.hpp"
 #include "sector.hpp"
+#include "camera.hpp"
 
 class LevelServer
 {
 protected:
 	std::vector<ShapeComponent> level_polygons;
 	std::vector<Sector> sectors;
+	std::vector<Camera*> cameras;
 	
 public:	
 	LevelServer();
 	virtual ~LevelServer();
 	
-	std::vector<ShapeComponent> get_levels_shapes();
+	void add_camera(Camera* camera);
+	Camera* get_camera(int index);
 	
 	void add_new_polygon(ShapeComponent new_polygon);
+	ShapeComponent get_levels_shape(int index);
+	std::vector<ShapeComponent> get_levels_shapes();
+	
 	void add_new_sector(Sector new_sector);
 	Sector get_sector_by_index(int index);
 };
